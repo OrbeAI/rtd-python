d15853c47c7dcebf7d470ad42871f9e38d463b86
OrbeAI <90585189+OrbeAI@users.noreply.github.com>
2021-09-16 23:24:17 -0300

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -33,5 +33,10 @@ html_theme = 'sphinx_rtd_theme'
 
 html_logo = 'logo_orbe.png'
 
+html_theme_options = {
+    'logo_only': True,
+    'display_version': False,
+}
+
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
