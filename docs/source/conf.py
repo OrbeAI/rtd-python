e833358196ab51cf2f9f556e74998d996129f26a
OrbeAI <90585189+OrbeAI@users.noreply.github.com>
2021-09-22 19:17:14 -0300

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -36,8 +36,7 @@ templates_path = ['_templates']
 
 # -- Options for HTML output
 
-#html_theme = 'sphinx_rtd_theme'
-html_theme = 'sphinx_book_theme'
+html_theme = 'sphinx_rtd_theme'
 
 html_logo = 'logo_orbe.png'
 
